a286412c04ac3f0fc11d7066e885aafcc4aa5cb4
Okami Wong <wtfimba@live.com>
2023-08-17 13:59:27 +0900

    format

--- playground/helloWorld.cu
+++ playground/helloWorld.cu
@@ -1,13 +1,13 @@
 #include <cstdio>
 
 __global__ void helloWorldFromGpu() {
-  if(blockIdx.x == 0 && threadIdx.x == 0)
+  if (blockIdx.x == 0 && threadIdx.x == 0)
     printf("GPU: Hello world.\n");
 }
 
 int main() {
   printf("CPU: Hello world.\n");
-  helloWorldFromGpu<<<1,1>>>();
+  helloWorldFromGpu<<<1, 1>>>();
   cudaDeviceSynchronize();
   return 0;
 }
