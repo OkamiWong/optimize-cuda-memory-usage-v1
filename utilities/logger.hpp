06b70a922512331ee79d80a44e9f3de2cc65722e
Okami Wong <wtfimba@live.com>
2023-08-18 18:25:54 +0900

    add logger.hpp

---
+++ utilities/logger.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdio>
+
+namespace logger {
+void log(const char* const file, const char* const function, const char* const info = nullptr) {
+  printf("[%s/%s]", file, function);
+  if (info != nullptr) {
+    printf(": %s\n", info);
+  } else {
+    printf("\n");
+  }
+}
+}  // namespace logger
+
+#define __FILENAME__ (strrchr("/" __FILE__, '/') + 1)
+
+#define LOG_TRACE() logger::log(__FILENAME__, __func__)
+
+#define LOG_TRACE_WITH_INFO(info) logger::log(__FILENAME__, __func__, info)
