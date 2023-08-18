a80a713b1459a4e629672704538787397c149d48
Okami Wong <wtfimba@live.com>
2023-08-18 18:32:59 +0900

    change logger's output format

--- utilities/logger.hpp
+++ utilities/logger.hpp
@@ -6,7 +6,7 @@ namespace logger {
 void log(const char* const file, const char* const function, const char* const info = nullptr) {
   printf("[%s/%s]", file, function);
   if (info != nullptr) {
-    printf(": %s\n", info);
+    printf(" %s\n", info);
   } else {
     printf("\n");
   }
