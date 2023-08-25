aff167e09797d796b65747f8c64fb05ee61ebdae
Okami Wong <wtfimba@live.com>
2023-08-25 22:53:12 +0900

    wip: taskManager

--- optimization/taskManager.cu
+++ optimization/taskManager.cu
@@ -1,5 +1,8 @@
 #include <cuda.h>
 
+#include <cassert>
+#include <cstdlib>
+#include <functional>
 #include <map>
 #include <memory>
 #include <queue>
@@ -7,6 +10,7 @@
 #include <vector>
 
 #include "../utilities/cudaUtilities.hpp"
+#include "../utilities/logger.hpp"
 #include "taskManager.hpp"
 
 TaskManager *TaskManager::instance = nullptr;
@@ -43,11 +47,56 @@ void extractGraphNodesAndEdges(
   }
 }
 
+void executeNode(CUgraphNode node) {
+  CUgraphNodeType nodeType;
+  checkCudaErrors(cuGraphNodeGetType(node, &nodeType));
+  if (nodeType == CU_GRAPH_NODE_TYPE_KERNEL) {
+    // TODO
+  } else if (nodeType == CU_GRAPH_NODE_TYPE_MEM_ALLOC) {
+    // TODO
+  } else if (nodeType == CU_GRAPH_NODE_TYPE_MEM_FREE) {
+    // TODO
+  } else if (nodeType == CU_GRAPH_NODE_TYPE_MEMSET) {
+    // TODO
+  } else {
+    LOG_TRACE_WITH_INFO("Unsupported node type: %d", nodeType);
+    exit(-1);
+  }
+}
+
 std::map<GraphNodeId, float> TaskManager::getKernelRunningTimes(cudaGraph_t graph) {
   std::vector<CUgraphNode> nodes;
   std::map<CUgraphNode, std::vector<CUgraphNode>> edges;
   extractGraphNodesAndEdges(graph, nodes, edges);
 
+  std::map<CUgraphNode, int> inDegrees;
+  for (auto &[u, outEdges] : edges) {
+    for (auto &v : outEdges) {
+      inDegrees[v] += 1;
+    }
+  }
+
+  typedef std::pair<int, CUgraphNode> RemainingNode;
+  std::priority_queue<RemainingNode, std::vector<RemainingNode>, std::greater<RemainingNode>> remainingNodes;
+  for (auto &node : nodes) {
+    remainingNodes.push(std::make_pair(inDegrees[node], node));
+  }
+
+  // Kahn Algorithm
   std::map<GraphNodeId, float> kernelRunningTimes;
+  CudaEventClock clock;
+
+  while (!remainingNodes.empty()) {
+    auto [inDegree, u] = remainingNodes.top();
+    assert(inDegree == 0);
+    remainingNodes.pop();
+
+    clock.start();
+    executeNode(u);
+    clock.end();
+    checkCudaErrors(cudaDeviceSynchronize());
+    kernelRunningTimes[reinterpret_cast<GraphNodeId>(u)] = clock.getTimeInSeconds();
+  }
+
   return kernelRunningTimes;
 }
