ac9215e34a2e71c972059e3134647e5228c599d1
Okami Wong <wtfimba@live.com>
2023-08-30 11:29:35 +0900

    remove case_chainOfGemms

--- userApplications/artificialApplication.cu
+++ userApplications/artificialApplication.cu
@@ -1,5 +1,3 @@
-#include <cublas_v2.h>
-
 #include <cassert>
 #include <cstdio>
 
@@ -9,118 +7,6 @@
 #include "../utilities/cudaUtilities.hpp"
 #include "../utilities/logger.hpp"
 #include "../utilities/utilities.hpp"
-
-namespace case_chainOfGemms {
-template <typename T>
-void fillRandomEntries(T *matrix, int m, int n, int lda) {
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      matrix[i * lda + j] = 2 * static_cast<T>(drand48()) - 1;
-    }
-  }
-}
-
-void tf32GemmUsingTensorCore(cublasHandle_t cublasHandle, int m, int n, int k, float *d_A, float *d_B, float *d_C) {
-  const float alpha = 1.0f;
-  const float beta = 0.0f;
-
-  checkCudaErrors(
-    cublasGemmEx(
-      cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha,
-      d_B, CUDA_R_32F, n,
-      d_A, CUDA_R_32F, k,
-      &beta,
-      d_C, CUDA_R_32F, n,
-      CUBLAS_COMPUTE_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP
-    )
-  );
-}
-
-void runChainOfGemms(bool useGraph = true) {
-  constexpr size_t CHAIN_LEN = 16;
-  constexpr size_t DIMENSION = 14 * (1 << 10);
-
-  // Calculate matrix dimensions
-  const int m = DIMENSION;
-  const int k = DIMENSION;
-  const int n = DIMENSION;
-  const size_t A_SIZE = m * k * sizeof(float);
-  const size_t B_SIZE = k * n * sizeof(float);
-  const size_t C_SIZE = m * n * sizeof(float);
-
-  // Initialzie
-  cublasHandle_t cublasHandle;
-  checkCudaErrors(cublasCreate(&cublasHandle));
-  checkCudaErrors(cublasSetMathMode(cublasHandle, CUBLAS_TENSOR_OP_MATH));
-
-  // Allocate memory
-  float *a[CHAIN_LEN], *b[CHAIN_LEN], *c[CHAIN_LEN];
-  for (int i = 0; i < CHAIN_LEN; i++) {
-    checkCudaErrors(cudaMallocManaged(&a[i], A_SIZE));
-    checkCudaErrors(cudaMallocManaged(&b[i], B_SIZE));
-    checkCudaErrors(cudaMallocManaged(&c[i], C_SIZE));
-  }
-
-  // Initialize memory
-  for (int i = 0; i < CHAIN_LEN; i++) {
-    fillRandomEntries(a[i], m, k, k);
-    fillRandomEntries(b[i], k, n, n);
-  }
-
-  CudaEventClock clock;
-
-  if (useGraph) {
-    cudaStream_t stream;
-    checkCudaErrors(cudaStreamCreate(&stream));
-    checkCudaErrors(cublasSetStream(cublasHandle, stream));
-    checkCudaErrors(cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal));
-
-    for (int i = 0; i < CHAIN_LEN; i++) {
-      annotateNextKernel({a[i], b[i]}, {c[i]}, stream);
-      tf32GemmUsingTensorCore(cublasHandle, m, n, k, a[i], b[i], c[i]);
-    }
-
-    checkCudaErrors(cudaGetLastError());
-
-    cudaGraph_t graph;
-    checkCudaErrors(cudaStreamEndCapture(stream, &graph));
-
-    auto taskManager = TaskManager::getInstance();
-    auto kernelRunningTimes = taskManager->getKernelRunningTimes(graph);
-    for (const auto &[id, time] : kernelRunningTimes) {
-      LOG_TRACE_WITH_INFO("%p: %.6f", id, time);
-    }
-
-    cudaGraphExec_t graphExec;
-    checkCudaErrors(cudaGraphInstantiate(&graphExec, graph, nullptr, nullptr, 0));
-
-    clock.start(stream);
-    checkCudaErrors(cudaGraphLaunch(graphExec, stream));
-    clock.end(stream);
-
-    checkCudaErrors(cudaStreamDestroy(stream));
-  } else {
-    clock.start();
-    for (int i = 0; i < CHAIN_LEN; i++) {
-      tf32GemmUsingTensorCore(cublasHandle, m, n, k, a[i], b[i], c[i]);
-    }
-    clock.end();
-  }
-
-  checkCudaErrors(cudaDeviceSynchronize());
-
-  LOG_TRACE_WITH_INFO("Total time used (s): %.2f", clock.getTimeInSeconds());
-
-  // Clean up
-  for (int i = 0; i < CHAIN_LEN; i++) {
-    checkCudaErrors(cudaFree(a[i]));
-    checkCudaErrors(cudaFree(b[i]));
-    checkCudaErrors(cudaFree(c[i]));
-  }
-
-  checkCudaErrors(cublasDestroy(cublasHandle));
-}
-}  // namespace case_chainOfGemms
 
 namespace case_chainOfStreams {
 template <typename T>
@@ -237,7 +123,6 @@ void runChainOfStreams(bool useGraph = true) {
 int main() {
   initializeCudaDevice();
 
-  // case_chainOfGemms::runChainOfGemms();
   case_chainOfStreams::runChainOfStreams();
 
   return 0;
