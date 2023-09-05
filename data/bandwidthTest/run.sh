46240ca558d73587019844ca11e941741e9afe8b
Okami Wong <wtfimba@live.com>
2023-09-05 19:39:33 +0900

    run data/bandwidthTest

--- data/bandwidthTest/run.sh
+++ data/bandwidthTest/run.sh
@@ -5,3 +5,5 @@ HEADER="kind,size(Byte),time(s),speed(GB/s)"
 echo $HEADER > $OUTPUT_FILE
 
 $BIN -no-header -start-size=100000 -end-size=8000000 -step-size=100000 >> $OUTPUT_FILE
+
+$BIN -no-header -use-unified-memory -start-size=100000 -end-size=8000000 -step-size=100000 >> $OUTPUT_FILE
