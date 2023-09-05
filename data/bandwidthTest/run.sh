b3f08979ece299752662854cdca35e610ae145d3
Okami Wong <wtfimba@live.com>
2023-09-05 19:56:38 +0900

    run data/bandwidthTest

--- data/bandwidthTest/run.sh
+++ data/bandwidthTest/run.sh
@@ -6,4 +6,4 @@ echo $HEADER > $OUTPUT_FILE
 
 $BIN -no-header -start-size=100000 -end-size=8000000 -step-size=100000 >> $OUTPUT_FILE
 
-$BIN -no-header -use-unified-memory -start-size=100000 -end-size=8000000 -step-size=100000 >> $OUTPUT_FILE
+$BIN -no-header -use-unified-memory -start-size=1000000 -end-size=80000000 -step-size=1000000 >> $OUTPUT_FILE
