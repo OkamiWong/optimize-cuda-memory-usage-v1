cf800faaa9a53d794ea00879631c677018472c57
Okami Wong <wtfimba@live.com>
2023-09-06 11:07:45 +0900

    implement bandwidthTest/testNvlinkBandwidth when not using unified memory

--- experiments/bandwidthTest.cu
+++ experiments/bandwidthTest.cu
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cassert>
 #include <limits>
 #include <memory>
 #include <sstream>
@@ -12,8 +13,9 @@
 #include "../utilities/utilities.hpp"
 
 constexpr int REPETITION = 100;
-constexpr int DEVICE_0_ID = 0;
-constexpr int DEVICE_1_ID = 1;
+constexpr int PCIE_DEVICE_ID = 0;
+constexpr int NVLINK_DEVICE_ID_A = 1;
+constexpr int NVLINK_DEVICE_ID_B = 2;
 
 void printHeader() {
   std::stringstream ss;
@@ -36,8 +38,89 @@ void printDataOfTheSameKind(const std::string &kind, const std::vector<size_t> &
   fputs(ss.str().c_str(), stdout);
 }
 
-void testNvlinkBandwidth(const std::vector<size_t> &sizes) {
-  // TODO
+void enablePeerAccessForNvlink() {
+  int canAccessPeerAToB, canAccessPeerBToA;
+  checkCudaErrors(cudaDeviceCanAccessPeer(&canAccessPeerAToB, NVLINK_DEVICE_ID_A, NVLINK_DEVICE_ID_B));
+  checkCudaErrors(cudaDeviceCanAccessPeer(&canAccessPeerBToA, NVLINK_DEVICE_ID_B, NVLINK_DEVICE_ID_A));
+
+  assert(canAccessPeerAToB);
+  assert(canAccessPeerBToA);
+
+  checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
+  checkCudaErrors(cudaDeviceEnablePeerAccess(NVLINK_DEVICE_ID_B, 0));
+  checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_B));
+  checkCudaErrors(cudaDeviceEnablePeerAccess(NVLINK_DEVICE_ID_A, 0));
+}
+
+void disablePeerAccessForNvlink() {
+  checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
+  checkCudaErrors(cudaDeviceDisablePeerAccess(NVLINK_DEVICE_ID_B));
+  checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_B));
+  checkCudaErrors(cudaDeviceDisablePeerAccess(NVLINK_DEVICE_ID_A));
+}
+
+template <typename T>
+__global__ void initializeArrayKernel(T *array, T initialValue, int count) {
+  const int i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (i < count) {
+    array[i] = initialValue;
+  }
+}
+
+void testNvlinkBandwidth(const std::vector<size_t> &sizes, bool useUnifiedMemory, bool noHeader) {
+  enablePeerAccessForNvlink();
+
+  cudaStream_t stream;
+  checkCudaErrors(cudaStreamCreate(&stream));
+
+  std::vector<float> deviceToDeviceTimes;
+
+  for (auto size : sizes) {
+    float minDeviceToDeviceTime = std::numeric_limits<float>::max();
+    if (useUnifiedMemory) {
+      // TODO
+    } else {
+      int *arrayOnDeviceA;
+      checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
+      cudaMalloc(&arrayOnDeviceA, size);
+      initializeArrayKernel<<<(size / 512) + 1, 512>>>(arrayOnDeviceA, 0, size / sizeof(int));
+      checkCudaErrors(cudaDeviceSynchronize());
+
+      int *arrayOnDeviceB;
+      checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_B));
+      cudaMalloc(&arrayOnDeviceB, size);
+      initializeArrayKernel<<<(size / 512) + 1, 512>>>(arrayOnDeviceB, 0, size / sizeof(int));
+      checkCudaErrors(cudaDeviceSynchronize());
+
+      CudaEventClock clock;
+      for (int i = 0; i < REPETITION; i++) {
+        clock.start(stream);
+        checkCudaErrors(cudaMemcpyAsync(arrayOnDeviceA, arrayOnDeviceB, size, cudaMemcpyDeviceToDevice, stream));
+        clock.end(stream);
+        checkCudaErrors(cudaStreamSynchronize(stream));
+        minDeviceToDeviceTime = std::min(minDeviceToDeviceTime, clock.getTimeInSeconds());
+      }
+
+      checkCudaErrors(cudaFree(arrayOnDeviceA));
+      checkCudaErrors(cudaFree(arrayOnDeviceB));
+
+      deviceToDeviceTimes.push_back(minDeviceToDeviceTime);
+    }
+  }
+
+  checkCudaErrors(cudaStreamDestroy(stream));
+
+  disablePeerAccessForNvlink();
+
+  if (!noHeader) {
+    printHeader();
+  }
+
+  if (useUnifiedMemory) {
+    printDataOfTheSameKind("NVLink-DeviceToDevice-UnifiedMemory", sizes, deviceToDeviceTimes);
+  } else {
+    printDataOfTheSameKind("NVLink-DeviceToDevice", sizes, deviceToDeviceTimes);
+  }
 }
 
 void testPcieBandwidth(const std::vector<size_t> &sizes, bool useUnifiedMemory, bool noHeader) {
@@ -61,7 +144,7 @@ void testPcieBandwidth(const std::vector<size_t> &sizes, bool useUnifiedMemory, 
 
       for (int i = 0; i < REPETITION; i++) {
         clock.start(stream);
-        checkCudaErrors(cudaMemPrefetchAsync(array, size, DEVICE_0_ID, stream));
+        checkCudaErrors(cudaMemPrefetchAsync(array, size, PCIE_DEVICE_ID, stream));
         clock.end(stream);
         checkCudaErrors(cudaStreamSynchronize(stream));
         minHostToDeviceTime = std::min(minHostToDeviceTime, clock.getTimeInSeconds());
@@ -148,7 +231,7 @@ int main(int argc, char **argv) {
   }
 
   if (useNvlink) {
-    testNvlinkBandwidth(sizes);
+    testNvlinkBandwidth(sizes, useUnifiedMemory, noHeader);
   } else {
     testPcieBandwidth(sizes, useUnifiedMemory, noHeader);
   }
