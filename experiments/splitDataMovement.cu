291b2098869f3bf24dacb24987c88cfcfcc61027
Okami Wong <wtfimba@live.com>
2023-09-07 11:22:40 +0900

    implement splitDataMovement

--- experiments/splitDataMovement.cu
+++ experiments/splitDataMovement.cu
@@ -17,21 +17,22 @@ constexpr int PCIE_DEVICE_ID = 0;
 constexpr int NVLINK_DEVICE_ID_A = 1;
 constexpr int NVLINK_DEVICE_ID_B = 2;
 
-constexpr int SPLIT = 2;
+constexpr int MAX_SPLIT = 16;
 
 void printHeader() {
   std::stringstream ss;
   auto csvWriter = csv::make_csv_writer(ss);
-  csvWriter << std::make_tuple("kind", "size(Byte)", "time(s)", "speed(GB/s)");
+  csvWriter << std::make_tuple("kind", "split", "size(Byte)", "time(s)", "speed(GB/s)");
   fputs(ss.str().c_str(), stdout);
 }
 
-void printDataOfTheSameKind(const std::string &kind, const std::vector<size_t> &sizes, const std::vector<float> &times) {
+void printDataOfTheSameKind(const std::string &kind, const std::vector<size_t> &sizes, const int split, const std::vector<float> &times) {
   std::stringstream ss;
   auto csvWriter = csv::make_csv_writer(ss);
   for (int i = 0; i < sizes.size(); i++) {
     csvWriter << std::make_tuple(
       kind,
+      split,
       sizes[i],
       toStringWithPrecision(times[i], 6),
       toStringWithPrecision(static_cast<float>(sizes[i]) / times[i] / 1e9, 3)
@@ -69,68 +70,91 @@ __global__ void initializeArrayKernel(T *array, T initialValue, size_t count) {
   }
 }
 
-void testNvlinkBandwidth(const std::vector<size_t> &sizes, bool useUnifiedMemory, bool noHeader) {
+void testNvlinkBandwidth(const std::vector<size_t> &sizes, int split, bool useUnifiedMemory, bool noHeader) {
   enablePeerAccessForNvlink();
 
-  cudaStream_t stream;
+  cudaStream_t streams[MAX_SPLIT];
   checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
-  checkCudaErrors(cudaStreamCreate(&stream));
+  for (int i = 0; i < split; i++) {
+    checkCudaErrors(cudaStreamCreate(&streams[i]));
+  }
 
   std::vector<float> deviceToDeviceTimes;
 
   for (auto size : sizes) {
+    assert(size % split == 0);
+    const size_t singleArraySize = size / split;
+
     float minDeviceToDeviceTime = std::numeric_limits<float>::max();
+
     if (useUnifiedMemory) {
-      int *array;
-      checkCudaErrors(cudaMallocManaged(&array, size));
+      int *array[MAX_SPLIT];
       checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
-      initializeArrayKernel<<<(size / 512) + 1, 512>>>(array, 0, size / sizeof(int));
+      for (int i = 0; i < split; i++) {
+        checkCudaErrors(cudaMallocManaged(&array[i], singleArraySize));
+        initializeArrayKernel<<<(singleArraySize / 512) + 1, 512>>>(array[i], 0, singleArraySize / sizeof(int));
+      }
       checkCudaErrors(cudaDeviceSynchronize());
 
       checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
       CudaEventClock clock;
       for (int i = 0; i < REPETITION; i++) {
-        clock.start(stream);
-        checkCudaErrors(cudaMemPrefetchAsync(array, size, i % 2 == 0 ? NVLINK_DEVICE_ID_B : NVLINK_DEVICE_ID_A, stream));
-        clock.end(stream);
-        checkCudaErrors(cudaStreamSynchronize(stream));
+        clock.start();
+        for (int j = 0; j < split; j++) {
+          checkCudaErrors(cudaMemPrefetchAsync(array[j], singleArraySize, i % 2 == 0 ? NVLINK_DEVICE_ID_B : NVLINK_DEVICE_ID_A, streams[j]));
+        }
+        clock.end();
+        checkCudaErrors(cudaDeviceSynchronize());
+
         minDeviceToDeviceTime = std::min(minDeviceToDeviceTime, clock.getTimeInSeconds());
       }
 
-      checkCudaErrors(cudaFree(array));
+      for (int i = 0; i < split; i++) {
+        checkCudaErrors(cudaFree(array[i]));
+      }
 
       deviceToDeviceTimes.push_back(minDeviceToDeviceTime);
     } else {
-      int *arrayOnDeviceA;
+      int *arrayOnDeviceA[MAX_SPLIT];
       checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
-      cudaMalloc(&arrayOnDeviceA, size);
-      initializeArrayKernel<<<(size / 512) + 1, 512>>>(arrayOnDeviceA, 0, size / sizeof(int));
+      for (int i = 0; i < split; i++) {
+        cudaMalloc(&arrayOnDeviceA[i], singleArraySize);
+        initializeArrayKernel<<<(singleArraySize / 512) + 1, 512>>>(arrayOnDeviceA[i], 0, singleArraySize / sizeof(int));
+      }
       checkCudaErrors(cudaDeviceSynchronize());
 
-      int *arrayOnDeviceB;
+      int *arrayOnDeviceB[MAX_SPLIT];
       checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_B));
-      cudaMalloc(&arrayOnDeviceB, size);
-      initializeArrayKernel<<<(size / 512) + 1, 512>>>(arrayOnDeviceB, 0, size / sizeof(int));
+      for (int i = 0; i < split; i++) {
+        cudaMalloc(&arrayOnDeviceB[i], singleArraySize);
+        initializeArrayKernel<<<(singleArraySize / 512) + 1, 512>>>(arrayOnDeviceB[i], 0, singleArraySize / sizeof(int));
+      }
       checkCudaErrors(cudaDeviceSynchronize());
 
       checkCudaErrors(cudaSetDevice(NVLINK_DEVICE_ID_A));
       CudaEventClock clock;
       for (int i = 0; i < REPETITION; i++) {
-        clock.start(stream);
-        checkCudaErrors(cudaMemcpyAsync(arrayOnDeviceA, arrayOnDeviceB, size, cudaMemcpyDeviceToDevice, stream));
-        clock.end(stream);
-        checkCudaErrors(cudaStreamSynchronize(stream));
+        clock.start();
+        for (int j = 0; j < split; j++) {
+          checkCudaErrors(cudaMemcpyAsync(arrayOnDeviceA[j], arrayOnDeviceB[j], singleArraySize, cudaMemcpyDeviceToDevice, streams[j]));
+        }
+        clock.end();
+        checkCudaErrors(cudaDeviceSynchronize());
         minDeviceToDeviceTime = std::min(minDeviceToDeviceTime, clock.getTimeInSeconds());
       }
 
-      checkCudaErrors(cudaFree(arrayOnDeviceA));
-      checkCudaErrors(cudaFree(arrayOnDeviceB));
+      for (int i = 0; i < split; i++) {
+        checkCudaErrors(cudaFree(arrayOnDeviceA[i]));
+        checkCudaErrors(cudaFree(arrayOnDeviceB[i]));
+      }
 
       deviceToDeviceTimes.push_back(minDeviceToDeviceTime);
     }
   }
 
-  checkCudaErrors(cudaStreamDestroy(stream));
+  for (int i = 0; i < split; i++) {
+    checkCudaErrors(cudaStreamDestroy(streams[i]));
+  }
 
   disablePeerAccessForNvlink();
 
@@ -139,83 +163,94 @@ void testNvlinkBandwidth(const std::vector<size_t> &sizes, bool useUnifiedMemory
   }
 
   if (useUnifiedMemory) {
-    printDataOfTheSameKind("NVLink-DeviceToDevice-UnifiedMemory", sizes, deviceToDeviceTimes);
+    printDataOfTheSameKind("NVLink-DeviceToDevice-UnifiedMemory-Split", sizes, split, deviceToDeviceTimes);
   } else {
-    printDataOfTheSameKind("NVLink-DeviceToDevice", sizes, deviceToDeviceTimes);
+    printDataOfTheSameKind("NVLink-DeviceToDevice-Split", sizes, split, deviceToDeviceTimes);
   }
 }
 
-void testPcieBandwidth(const std::vector<size_t> &sizes, bool useUnifiedMemory, bool noHeader) {
+void testPcieBandwidth(const std::vector<size_t> &sizes, int split, bool useUnifiedMemory, bool noHeader) {
   std::vector<float> hostToDeviceTimes, deviceToHostTimes;
 
   warmUpCudaDevice();
 
-  cudaStream_t stream;
-  checkCudaErrors(cudaStreamCreate(&stream));
+  cudaStream_t streams[MAX_SPLIT];
+  for (int i = 0; i < split; i++) {
+    checkCudaErrors(cudaStreamCreate(&streams[i]));
+  }
 
   for (auto size : sizes) {
+    assert(size % split == 0);
+    const size_t singleArraySize = size / split;
+
     float minHostToDeviceTime = std::numeric_limits<float>::max();
     float minDeviceToHostTime = std::numeric_limits<float>::max();
 
     if (useUnifiedMemory) {
-      int *array;
-      checkCudaErrors(cudaMallocManaged(&array, size));
-      memset(array, 0, size);
+      int *array[MAX_SPLIT];
+      for (int i = 0; i < split; i++) {
+        checkCudaErrors(cudaMallocManaged(&array[i], singleArraySize));
+        memset(array[i], 0, singleArraySize);
+      }
 
       CudaEventClock clock;
 
       for (int i = 0; i < REPETITION; i++) {
-        clock.start(stream);
-        checkCudaErrors(cudaMemPrefetchAsync(array, size, PCIE_DEVICE_ID, stream));
-        clock.end(stream);
-        checkCudaErrors(cudaStreamSynchronize(stream));
+        clock.start();
+        for (int j = 0; j < split; j++) {
+          checkCudaErrors(cudaMemPrefetchAsync(array[j], singleArraySize, PCIE_DEVICE_ID, streams[j]));
+        }
+        clock.end();
+        checkCudaErrors(cudaDeviceSynchronize());
         minHostToDeviceTime = std::min(minHostToDeviceTime, clock.getTimeInSeconds());
 
-        clock.start(stream);
-        checkCudaErrors(cudaMemPrefetchAsync(array, size, cudaCpuDeviceId));
-        clock.end(stream);
-        checkCudaErrors(cudaStreamSynchronize(stream));
+        clock.start();
+        for (int j = 0; j < split; j++) {
+          checkCudaErrors(cudaMemPrefetchAsync(array[j], singleArraySize, cudaCpuDeviceId, streams[j]));
+        }
+        clock.end();
+        checkCudaErrors(cudaDeviceSynchronize());
         minDeviceToHostTime = std::min(minDeviceToHostTime, clock.getTimeInSeconds());
       }
 
-      checkCudaErrors(cudaFree(array));
+      for (int i = 0; i < split; i++) {
+        checkCudaErrors(cudaFree(array[i]));
+      }
 
       hostToDeviceTimes.push_back(minHostToDeviceTime);
       deviceToHostTimes.push_back(minDeviceToHostTime);
     } else {
-      assert(size % SPLIT == 0);
-
-      int *hostArray[SPLIT];
-      for (int i = 0; i < SPLIT; i++) {
-        checkCudaErrors(cudaMallocHost(&hostArray[i], size / SPLIT));
-        memset(hostArray[i], 0, size / SPLIT);
+      int *hostArray[MAX_SPLIT];
+      for (int i = 0; i < split; i++) {
+        checkCudaErrors(cudaMallocHost(&hostArray[i], singleArraySize));
+        memset(hostArray[i], 0, singleArraySize);
       }
 
-      int *deviceArray[SPLIT];
-      for (int i = 0; i < SPLIT; i++) {
-        checkCudaErrors(cudaMalloc(&deviceArray[i], size / SPLIT));
+      int *deviceArray[MAX_SPLIT];
+      for (int i = 0; i < split; i++) {
+        checkCudaErrors(cudaMalloc(&deviceArray[i], singleArraySize));
       }
 
       CudaEventClock clock;
       for (int i = 0; i < REPETITION; i++) {
-        clock.start(stream);
-        for (int j = 0; j < SPLIT; j++) {
-          checkCudaErrors(cudaMemcpyAsync(deviceArray[j], hostArray[j], size / SPLIT, cudaMemcpyHostToDevice, stream));
+        clock.start();
+        for (int j = 0; j < split; j++) {
+          checkCudaErrors(cudaMemcpyAsync(deviceArray[j], hostArray[j], singleArraySize, cudaMemcpyHostToDevice, streams[j]));
         }
-        clock.end(stream);
-        checkCudaErrors(cudaStreamSynchronize(stream));
+        clock.end();
+        checkCudaErrors(cudaDeviceSynchronize());
         minHostToDeviceTime = std::min(minHostToDeviceTime, clock.getTimeInSeconds());
 
-        clock.start(stream);
-        for (int j = 0; j < SPLIT; j++) {
-          checkCudaErrors(cudaMemcpyAsync(hostArray[j], deviceArray[j], size / SPLIT, cudaMemcpyDeviceToHost, stream));
+        clock.start();
+        for (int j = 0; j < split; j++) {
+          checkCudaErrors(cudaMemcpyAsync(hostArray[j], deviceArray[j], singleArraySize, cudaMemcpyDeviceToHost, streams[j]));
         }
-        clock.end(stream);
-        checkCudaErrors(cudaStreamSynchronize(stream));
+        clock.end();
+        checkCudaErrors(cudaDeviceSynchronize());
         minDeviceToHostTime = std::min(minDeviceToHostTime, clock.getTimeInSeconds());
       }
 
-      for (int i = 0; i < SPLIT; i++) {
+      for (int i = 0; i < split; i++) {
         checkCudaErrors(cudaFreeHost(hostArray[i]));
         checkCudaErrors(cudaFree(deviceArray[i]));
       }
@@ -225,18 +260,20 @@ void testPcieBandwidth(const std::vector<size_t> &sizes, bool useUnifiedMemory, 
     }
   }
 
-  checkCudaErrors(cudaStreamDestroy(stream));
+  for (int i = 0; i < split; i++) {
+    checkCudaErrors(cudaStreamDestroy(streams[i]));
+  }
 
   if (!noHeader) {
     printHeader();
   }
 
   if (useUnifiedMemory) {
-    printDataOfTheSameKind("PCIe-HostToDevice-UnifiedMemory", sizes, hostToDeviceTimes);
-    printDataOfTheSameKind("PCIe-DeviceToHost-UnifiedMemory", sizes, deviceToHostTimes);
+    printDataOfTheSameKind("PCIe-HostToDevice-UnifiedMemory-Split", sizes, split, hostToDeviceTimes);
+    printDataOfTheSameKind("PCIe-DeviceToHost-UnifiedMemory-Split", sizes, split, deviceToHostTimes);
   } else {
-    printDataOfTheSameKind("PCIe-HostToDevice-Split", sizes, hostToDeviceTimes);
-    printDataOfTheSameKind("PCIe-DeviceToHost-Split", sizes, deviceToHostTimes);
+    printDataOfTheSameKind("PCIe-HostToDevice-Split", sizes, split, hostToDeviceTimes);
+    printDataOfTheSameKind("PCIe-DeviceToHost-Split", sizes, split, deviceToHostTimes);
   }
 }
 
@@ -253,6 +290,10 @@ int main(int argc, char **argv) {
   cmdl("end-size", 100'000'000ull) >> endSize;     // 100 MB
   cmdl("step-size", 10'000'000ull) >> stepSize;    // 10 MB
 
+  int split;
+  cmdl("split", 2) >> split;  // Split the data into two arrays and enqueue them into one stream by default
+  assert(split < MAX_SPLIT);
+
   std::vector<size_t> sizes;
   size_t s = startSize;
   while (s <= endSize) {
@@ -265,9 +306,9 @@ int main(int argc, char **argv) {
   }
 
   if (useNvlink) {
-    testNvlinkBandwidth(sizes, useUnifiedMemory, noHeader);
+    testNvlinkBandwidth(sizes, split, useUnifiedMemory, noHeader);
   } else {
-    testPcieBandwidth(sizes, useUnifiedMemory, noHeader);
+    testPcieBandwidth(sizes, split, useUnifiedMemory, noHeader);
   }
 
   return 0;
