523fcac3d937fc0627df7b34a2eaf61ea0adc462
Okami Wong <wtfimba@live.com>
2023-09-15 10:56:05 +0900

    remove optExample

--- playground/z3Playground.cpp
+++ playground/z3Playground.cpp
@@ -6,28 +6,6 @@
 #include <string>
 #include <vector>
 
-void optExample() {
-  z3::context c;
-  z3::optimize opt(c);
-  z3::params p(c);
-  p.set("priority", c.str_symbol("pareto"));
-  opt.set(p);
-  z3::expr x = c.int_const("x");
-  z3::expr y = c.int_const("y");
-  opt.add(10 >= x && x >= 0);
-  opt.add(10 >= y && y >= 0);
-  opt.add(x + y <= 11);
-  z3::optimize::handle h1 = opt.maximize(x);
-  z3::optimize::handle h2 = opt.maximize(y);
-  while (true) {
-    if (z3::check_result::sat == opt.check()) {
-      std::cout << x << ": " << opt.lower(h1) << " " << y << ": " << opt.lower(h2) << "\n";
-    } else {
-      break;
-    }
-  }
-}
-
 void longestPathExample() {
   // Adjacency matrix
   std::vector<std::vector<double>> dis = {
@@ -77,7 +55,6 @@ void longestPathExample() {
 }
 
 int main() {
-  // optExample();
   longestPathExample();
   return 0;
 }
