7206e261774b707d89539e1541032ab7b94682a5
Okami Wong <wtfimba@live.com>
2023-09-15 10:46:56 +0900

    add longestPathExample

--- playground/z3Playground.cpp
+++ playground/z3Playground.cpp
@@ -1,7 +1,9 @@
 #include <z3++.h>
 
+#include <cmath>
 #include <iostream>
-#include <sstream>
+#include <limits>
+#include <string>
 #include <vector>
 
 void optExample() {
@@ -26,7 +28,56 @@ void optExample() {
   }
 }
 
+void longestPathExample() {
+  // Adjacency matrix
+  std::vector<std::vector<double>> dis = {
+    {0, 1, 3},
+    {0, 0, 3},
+    {0, 0, 0}
+  };
+
+  const int n = dis.size();
+
+  z3::context context;
+  z3::optimize optimize(context);
+
+  std::vector<z3::expr> z;
+  std::vector<std::string> zNames;
+
+  for (int i = 0; i < n; i++) {
+    zNames.push_back("z[" + std::to_string(i) + "]");
+    if (i == 0) {
+      z.push_back(context.real_val(0));
+    } else {
+      z.push_back(context.real_const(zNames[i].c_str()));
+    }
+  }
+
+  auto minusInfinity = context.real_val(-(0x7fffffff));
+
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      std::cout << i << ", " << j << ": " << dis[j][i] << std::endl;
+      if (fabs(dis[j][i]) <= std::numeric_limits<double>::epsilon()) {
+        optimize.add(z[i] >= z[j] + minusInfinity);
+      } else {
+        optimize.add(z[i] >= z[j] + context.real_val(std::to_string(dis[j][i]).c_str()));
+      }
+    }
+  }
+
+  std::cout << optimize << std::endl;
+
+  auto handle = optimize.minimize(z[n - 1]);
+  if (optimize.check() == z3::check_result::sat) {
+    std::cout << optimize.lower(handle) << std::endl;
+  } else {
+    std::cout << "No solution found" << std::endl;
+  }
+}
+
 int main() {
-  optExample();
+  // optExample();
+  longestPathExample();
   return 0;
 }
