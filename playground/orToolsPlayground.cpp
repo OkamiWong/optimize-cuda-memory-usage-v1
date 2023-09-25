76feec49cc026be1321d7f8d0f4835d8898d911b
Okami Wong <wtfimba@live.com>
2023-09-25 16:28:41 +0900

    add basic example

--- playground/orToolsPlayground.cpp
+++ playground/orToolsPlayground.cpp
@@ -3,6 +3,42 @@
 
 using namespace operations_research;
 
+namespace operations_research {
+void BasicExample() {
+  // Create the linear solver with the GLOP backend.
+  std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("GLOP"));
+
+  // Create the variables x and y.
+  MPVariable *const x = solver->MakeNumVar(0.0, 1, "x");
+  MPVariable *const y = solver->MakeNumVar(0.0, 2, "y");
+  auto z = solver->MakeBoolVar("z");
+
+  LOG(INFO) << "Number of variables = " << solver->NumVariables();
+
+  // Create a linear constraint, 0 <= x + y <= 2.
+  MPConstraint *const ct = solver->MakeRowConstraint(0.0, 2.0, "ct");
+  ct->SetCoefficient(x, 1);
+  ct->SetCoefficient(y, 1);
+
+  LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
+
+  // Create the objective function, 3 * x + y.
+  MPObjective *const objective = solver->MutableObjective();
+  objective->SetCoefficient(x, 3);
+  objective->SetCoefficient(y, 1);
+  objective->SetCoefficient(z, 1);
+  objective->SetMaximization();
+
+  solver->Solve();
+
+  LOG(INFO) << "Solution:" << std::endl;
+  LOG(INFO) << "Objective value = " << objective->Value();
+  LOG(INFO) << "x = " << x->solution_value();
+  LOG(INFO) << "y = " << y->solution_value();
+  LOG(INFO) << "z = " << z->solution_value();
+}
+}  // namespace operations_research
+
 struct TwoStepIntegerProgrammingStrategy {
   // Experiment setup
   static constexpr int NUMBER_OF_KERNELS = 4;
@@ -486,6 +522,8 @@ struct TwoStepIntegerProgrammingStrategy {
 };
 
 int main() {
+  operations_research::BasicExample();
+
   auto twoStepIntegerProgrammingStrategy = std::make_unique<TwoStepIntegerProgrammingStrategy>();
   twoStepIntegerProgrammingStrategy->runSecondStepExample();
 
