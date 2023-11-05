95021f09fcf0dfbeb481eac193e4acd328aed50e
Okami Wong <wtfimba@live.com>
2023-11-05 21:36:16 +0900

    (tiledCholesky.cu) add verification

--- playground/tiledCholesky.cu
+++ playground/tiledCholesky.cu
@@ -2,46 +2,87 @@
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 #include <cusolverDn.h>
+#include <fmt/core.h>
 
+#include <cmath>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <limits>
+#include <memory>
 
 #include "../utilities/cudaUtilities.hpp"
 
 const int N = 8;
 
 // Credit to: https://math.stackexchange.com/questions/357980/how-to-generate-random-symmetric-positive-definite-matrices-using-matlab
-void generateRandomSymmetricPositiveDefiniteMatrix(double *h_A, const int N) {
+void generateRandomSymmetricPositiveDefiniteMatrix(double *h_A, const int n) {
   // --- Initialize random seed
   srand(time(NULL));
 
-  double *h_A_temp = (double *)malloc(N * N * sizeof(double));
+  double *h_A_temp = (double *)malloc(n * n * sizeof(double));
 
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      h_A_temp[i * N + j] = (float)rand() / (float)RAND_MAX;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      h_A_temp[i * n + j] = (float)rand() / (float)RAND_MAX;
 
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      h_A[i * N + j] = 0.5 * (h_A_temp[i * N + j] + h_A_temp[j * N + i]);
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      h_A[i * n + j] = 0.5 * (h_A_temp[i * n + j] + h_A_temp[j * n + i]);
 
-  for (int i = 0; i < N; i++) h_A[i * N + i] = h_A[i * N + i] + N;
+  for (int i = 0; i < n; i++) h_A[i * n + i] = h_A[i * n + i] + n;
 }
 
 void printSquareMatrix(double *h_A, const int N) {
-  auto originalWidth = std::cout.width();
-  std::cout << std::setw(6);
-
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       if (j != 0) std::cout << " ";
-      std::cout << h_A[i * N + j];
+      std::cout << std::setw(6) << std::setprecision(3) << h_A[i * N + j];
     }
     std::cout << std::endl;
   }
+}
 
-  std::cout << std::setw(originalWidth);
+void cleanCusolverCholeskyDecompositionResult(double *L, const int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      L[i + j * n] = 0;
+      std::swap(L[i + j * n], L[i * n + j]);
+    }
+  }
+}
+
+bool verifyCholeskyDecomposition(double *A, double *L, const int n) {
+  auto newA = std::make_unique<double[]>(n * n);
+  memset(newA.get(), 0, n * n * sizeof(double));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      for (int k = 0; k < n; k++) {
+        newA[i * n + j] += L[i * n + k] * L[k + j * n];
+      }
+    }
+  }
+
+  double error = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      error += fabs(A[i * n + j] - newA[i * n + j]);
+    }
+  }
+
+  fmt::print("A:\n");
+  printSquareMatrix(A, n);
+
+  fmt::print("\nnewA:\n");
+  printSquareMatrix(newA.get(), n);
+
+  fmt::print("\nL:\n");
+  printSquareMatrix(L, n);
+  fmt::print("\n");
+
+  fmt::print("error = {:.6f}\n", error);
+
+  return error <= 1e-6;
 }
 
 int main() {
@@ -51,6 +92,7 @@ int main() {
   cublasHandle_t cublas_handle;
   checkCudaErrors(cublasCreate(&cublas_handle));
 
+  // Init
   double *h_A = (double *)malloc(N * N * sizeof(double));
   generateRandomSymmetricPositiveDefiniteMatrix(h_A, N);
 
@@ -58,18 +100,19 @@ int main() {
   checkCudaErrors(cudaMalloc(&d_A, N * N * sizeof(double)));
   checkCudaErrors(cudaMemcpy(d_A, h_A, N * N * sizeof(double), cudaMemcpyHostToDevice));
 
-  // cuSOLVE input/output parameters/arrays
   int work_size = 0;
+  checkCudaErrors(cusolverDnDpotrf_bufferSize(solver_handle, CUBLAS_FILL_MODE_LOWER, N, d_A, N, &work_size));
+
+  double *work;
+  checkCudaErrors(cudaMalloc(&work, work_size * sizeof(double)));
+
   int *devInfo;
   checkCudaErrors(cudaMalloc(&devInfo, sizeof(int)));
 
-  // CUDA CHOLESKY initialization
-  checkCudaErrors(cusolverDnDpotrf_bufferSize(solver_handle, CUBLAS_FILL_MODE_LOWER, N, d_A, N, &work_size));
-
-  // CUDA POTRF execution
-  double *work;
-  checkCudaErrors(cudaMalloc(&work, work_size * sizeof(double)));
+  // Calculate
   checkCudaErrors(cusolverDnDpotrf(solver_handle, CUBLAS_FILL_MODE_LOWER, N, d_A, N, work, work_size, devInfo));
+
+  // Check
   int devInfo_h = 0;
   checkCudaErrors(cudaMemcpy(&devInfo_h, devInfo, sizeof(int), cudaMemcpyDeviceToHost));
   if (devInfo_h != 0) {
@@ -77,10 +120,13 @@ int main() {
               << "devInfo = " << devInfo_h << "\n\n";
   }
 
-  // At this point, the lower triangular part of A contains the elements of L.
-  checkCudaErrors(cudaMemcpy(h_A, d_A, N * N * sizeof(double), cudaMemcpyDeviceToHost));
-  printSquareMatrix(h_A, N);
+  // Verify
+  double *h_L = (double *)malloc(N * N * sizeof(double));
+  checkCudaErrors(cudaMemcpy(h_L, d_A, N * N * sizeof(double), cudaMemcpyDeviceToHost));
+  cleanCusolverCholeskyDecompositionResult(h_L, N);
+  fmt::print("Result passes verification: {}\n", verifyCholeskyDecomposition(h_A, h_L, N));
 
+  // Clean
   checkCudaErrors(cusolverDnDestroy(solver_handle));
 
   return 0;
