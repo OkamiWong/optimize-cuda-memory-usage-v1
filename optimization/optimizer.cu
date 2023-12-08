7006c9a14f5671bf0d2447f4dbc6e268fcf607e9
Okami Wong <wtfimba@live.com>
2023-12-08 21:36:15 +0900

    add design

--- optimization/optimizer.cu
+++ optimization/optimizer.cu
@@ -17,10 +17,29 @@ CustomGraph Optimizer::profileAndOptimize(cudaGraph_t originalGraph) {
   // Profile
   auto taskManager = TaskManager::getInstance();
   taskManager->registerDummyKernelHandle(originalGraph);
-  auto cuGraphNodeToKernelDurationMap = taskManager->getCuGraphNodeToKernelDurationMap(originalGraph);
+
+  // Find out nodes that run concurrently
+  // Design:
+  // - Use CUPTI Activity API to obtain node starting times and end times
+  // - Merge nodes that overlap in timeline
+  // - Use Disjoint Set to maintain the set relations
+  // Output:
+  // - Disjoint Set: map<cudaGraphNode_t, cudaGraphNode_t> nodeToDisjointSetRootMap
+
+  // Merge nodes that run concurrently
+  // Design:
+  // - Iterate through all nodes, merge each node with their root in the Disjoint Set
+  // Output:
+  // - 
+
+  // Find the duration of nodes 
+  // Design:
+  // - Reuse the node starting times and end times obtained in the first step
+  // - The duration of a logical node is the maximum of end times of corresponding actual nodes minus the minimum of starting times
 
   // Optimize
-  auto customGraph = this->optimize<PrefetchOnlyStrategy>(originalGraph, cuGraphNodeToKernelDurationMap);
+  // Design:
+  // - Integer Programming solved via Z3 / OR-Tools (SCIP, GLPK, ...)
 
   return customGraph;
 }
