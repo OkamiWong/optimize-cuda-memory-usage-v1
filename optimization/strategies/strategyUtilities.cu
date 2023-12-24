125eff240f95fe594ec92f97827839a2e7f8f381
Okami Wong <wtfimba@live.com>
2023-12-24 23:04:25 +0900

    feat(strategyUtilities.cu/printOptimizationInput): print logical nodes

--- optimization/strategies/strategyUtilities.cu
+++ optimization/strategies/strategyUtilities.cu
@@ -1,5 +1,39 @@
+#include <fmt/core.h>
+
+#include <string>
+
 #include "strategyUtilities.hpp"
 
-void printOptimizationInput(OptimizationInput &input) {
+inline std::string getLogicalNodeName(OptimizationInput::NodeId index) {
+  return fmt::format("logical_node_{}", index);
+}
 
+void printEdges(OptimizationInput &input) {
+  for (const auto &[u, destinations] : input.edges) {
+    for (auto v : destinations) {
+      fmt::print("{} -> {}\n", getLogicalNodeName(u), getLogicalNodeName(v));
+    }
+  }
+}
+
+void printLogicalNode(int index, OptimizationInput::LogicalNode &node) {
+  fmt::print("subgraph {} {\n", getLogicalNodeName(index));
+  fmt::print("label=\"{}\"\n", getLogicalNodeName(index));
+  fmt::print("}\n");
+}
+
+void printLogicalNodes(OptimizationInput &input) {
+  for (int i = 0; i < input.nodes.size(); i++) {
+    printLogicalNode(i, input.nodes[i]);
+  }
+}
+
+void printOptimizationInput(OptimizationInput &input) {
+  fmt::print("graph G {\n");
+
+  printLogicalNodes(input);
+
+  printEdges(input);
+
+  fmt::print("}\n");
 }
