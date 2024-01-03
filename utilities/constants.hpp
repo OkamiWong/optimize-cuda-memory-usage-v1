bd8a40ca532cfb8c2663ba4ea147c88d862af720
Okami Wong <wtfimba@live.com>
2024-01-03 18:19:36 +0900

    feat(constants.hpp): use namespace instead of static struct property

--- utilities/constants.hpp
+++ utilities/constants.hpp
@@ -1,6 +1,8 @@
-struct Constants {
-  static constexpr int DEVICE_ID = 0;
-  static constexpr float PREFETCHING_BANDWIDTH_IN_GBPS = 281.0;
-  static constexpr float PREFETCHING_BANDWIDTH = PREFETCHING_BANDWIDTH_IN_GBPS * 1e9;
-  static constexpr float OFFLOADING_BANDWIDTH = PREFETCHING_BANDWIDTH;
-};
+#pragma once
+
+namespace Constants {
+constexpr int DEVICE_ID = 0;
+constexpr float PREFETCHING_BANDWIDTH_IN_GBPS = 281.0;
+constexpr float PREFETCHING_BANDWIDTH = PREFETCHING_BANDWIDTH_IN_GBPS * 1e9;
+constexpr float OFFLOADING_BANDWIDTH = PREFETCHING_BANDWIDTH;
+};  // namespace Constants
