283118c5dcfdd06be89d3ebe387b0bff130c36f2
Okami Wong <wtfimba@live.com>
2024-01-04 12:32:39 +0900

    feat: revise memoryManager to remove managedMemorySpacesInitiallyOnDevice but add managedMemoryAddressToIndexMap

--- profiling/memoryManager.hpp
+++ profiling/memoryManager.hpp
@@ -5,13 +5,15 @@
 #include <set>
 
 struct MemoryManager {
+  inline static int managedMemoryAddressCount = 0;
   inline static std::map<void *, size_t> managedMemoryAddressToSizeMap;
-  inline static std::set<void *> managedMemorySpacesInitiallyOnDevice;
+  inline static std::map<void *, int> managedMemoryAddressToIndexMap;
 };
 
 template <typename T>
 __host__ void registerManagedMemoryAddress(T *devPtr, size_t size) {
   MemoryManager::managedMemoryAddressToSizeMap[static_cast<void *>(devPtr)] = size;
+  MemoryManager::managedMemoryAddressToIndexMap[static_cast<void *>(devPtr)] = MemoryManager::managedMemoryAddressCount++;
 }
 
 template <typename T>
@@ -20,7 +22,3 @@ __host__ cudaError_t wrappedCudaMallocManaged(T **devPtr, size_t size) {
   registerManagedMemoryAddress(*devPtr, size);
   return err;
 }
-
-void markMemorySpaceInitiallyOnDevice(void *devPtr);
-
-void markMemorySpaceInitiallyOnDevice(std::initializer_list<void *> devPtrs);
