4b696c24f809c4e4f603d435948dac3c1e082d01
Okami Wong <wtfimba@live.com>
2024-01-14 16:46:16 +0900

    feat(constants): increase ACCEPTABLE_RUNNING_TIME_FACTOR to 3

--- utilities/constants.hpp
+++ utilities/constants.hpp
@@ -5,5 +5,5 @@ constexpr int DEVICE_ID = 0;
 constexpr float PREFETCHING_BANDWIDTH_IN_GBPS = 281.0;
 constexpr float PREFETCHING_BANDWIDTH = PREFETCHING_BANDWIDTH_IN_GBPS * 1e9;
 constexpr float OFFLOADING_BANDWIDTH = PREFETCHING_BANDWIDTH;
-constexpr float ACCEPTABLE_RUNNING_TIME_FACTOR = 2;
+constexpr float ACCEPTABLE_RUNNING_TIME_FACTOR = 3;
 };  // namespace Constants
