0dcca53f3df0d955c330555712a28fa7ca4df737
Okami Wong <wtfimba@live.com>
2024-01-19 21:50:22 +0900

    feat(TaskAnnotation): make sure its total size is less than 4096 bytes

--- profiling/annotation.hpp
+++ profiling/annotation.hpp
@@ -6,7 +6,7 @@
 struct TaskAnnotation {
   // Before CUDA 12.1, the size of the parameters passed to a kernel is
   // limited to 4096 bytes. CUDA 12.1 increases this limit to 32764 bytes.
-  static constexpr size_t MAX_NUM_PTR = 4096 / 8 / 2;
+  static constexpr size_t MAX_NUM_PTR = (4096 - 4) / 8 / 2;
 
   int taskId;
 
