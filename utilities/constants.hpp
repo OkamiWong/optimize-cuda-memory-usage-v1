fc46d599b310f9e15f3f8aaf479859415c901216
Okami Wong <wtfimba@live.com>
2024-01-30 11:42:07 +0900

    feat(Constants): switch to memkf03

--- utilities/constants.hpp
+++ utilities/constants.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace Constants {
-constexpr int DEVICE_ID = 0;
-constexpr int STORAGE_DEVICE_ID = 1;
+constexpr int DEVICE_ID = 1;
+constexpr int STORAGE_DEVICE_ID = 2;
 };  // namespace Constants
