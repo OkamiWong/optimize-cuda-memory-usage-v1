6daf02931579cafc2b3a9e6196974aedde8748b4
Okami Wong <wtfimba@live.com>
2024-02-25 20:27:12 +0900

    refactor(memoryManager): avoid library compilation

--- profiling/memoryManager.hpp
+++ profiling/memoryManager.hpp
@@ -35,4 +35,29 @@ void registerApplicationOutput(T *devPtr) {
   MemoryManager::applicationOutputs.insert(static_cast<void *>(devPtr));
 }
 
-void updateManagedMemoryAddress(const std::map<void *, void *> oldAddressToNewAddressMap);
+inline void updateManagedMemoryAddress(const std::map<void *, void *> oldAddressToNewAddressMap) {
+  auto oldManagedMemoryAddressToSizeMap = MemoryManager::managedMemoryAddressToSizeMap;
+
+  MemoryManager::managedMemoryAddressToSizeMap.clear();
+  MemoryManager::managedMemoryAddressToIndexMap.clear();
+
+  for (int i = 0; i < MemoryManager::managedMemoryAddresses.size(); i++) {
+    assert(oldAddressToNewAddressMap.count(MemoryManager::managedMemoryAddresses[i]) == 1);
+
+    const auto newAddr = oldAddressToNewAddressMap.at(MemoryManager::managedMemoryAddresses[i]);
+    const auto oldAddr = MemoryManager::managedMemoryAddresses[i];
+
+    MemoryManager::managedMemoryAddresses[i] = newAddr;
+    MemoryManager::managedMemoryAddressToSizeMap[newAddr] = oldManagedMemoryAddressToSizeMap[oldAddr];
+    MemoryManager::managedMemoryAddressToIndexMap[newAddr] = i;
+
+    if (MemoryManager::applicationInputs.count(oldAddr) > 0) {
+      MemoryManager::applicationInputs.erase(oldAddr);
+      MemoryManager::applicationInputs.insert(newAddr);
+    }
+    if (MemoryManager::applicationOutputs.count(oldAddr) > 0) {
+      MemoryManager::applicationOutputs.erase(oldAddr);
+      MemoryManager::applicationOutputs.insert(newAddr);
+    }
+  }
+}
