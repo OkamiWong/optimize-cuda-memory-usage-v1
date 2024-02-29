2d296f75bbe406df2b4ad9e468db0da43b8b9e78
Okami Wong <wtfimba@live.com>
2024-02-29 11:24:22 +0900

    feat(lulesh): log average time per iteration

--- userApplications/lulesh/lulesh.cu
+++ userApplications/lulesh/lulesh.cu
@@ -3179,9 +3179,10 @@ void VerifyAndWriteFinalOutput(Real_t elapsed_time, Domain& locDom, Int_t nx, In
   printf("        MaxRelDiff   = %12.6e\n\n", MaxRelDiff);
 
   // Timing information
-  printf("\nElapsed time         = %10.2f (s)\n", elapsed_time);
-  printf("Grind time (us/z/c)  = %10.8g (per dom)  (%10.8g overall)\n", grindTime1, grindTime2);
-  printf("FOM                  = %10.8g (z/s)\n\n", 1000.0 / grindTime2);  // zones per second
+  printf("\nElapsed time               = %10.2f (s)\n", elapsed_time);
+  printf("Average time per iteration = %10.2f (s)\n", elapsed_time / *locDom.cycle_h);
+  printf("Grind time (us/z/c)        = %10.8g (per dom)  (%10.8g overall)\n", grindTime1, grindTime2);
+  printf("FOM                        = %10.8g (z/s)\n\n", 1000.0 / grindTime2);  // zones per second
 
   bool write_solution_flag = false;
   if (write_solution_flag) {
