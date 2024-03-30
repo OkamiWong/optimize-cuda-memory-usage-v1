5a11e4cfc09c4cb6a3c33fa29af2b7e17daa5048
Okami Wong <wtfimba@live.com>
2024-03-30 11:22:27 +0900

    feat(public): add header and library

---
+++ public/memopt.cu
@@ -0,0 +1,5 @@
+#include "memopt.hpp"
+
+void helloWorld() {
+  std::cout << "Hello world from memopt." << std::endl;
+}
---
+++ public/memopt.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../optimization/optimization.hpp"
+#include "../profiling/annotation.hpp"
+#include "../profiling/memoryManager.hpp"
+#include "../profiling/peakMemoryUsageProfiler.hpp"
+#include "../utilities/configurationManager.hpp"
+#include "../utilities/constants.hpp"
+#include "../utilities/cudaUtilities.hpp"
+#include "../utilities/logger.hpp"
+#include "../utilities/utilities.hpp"
+
+namespace memopt {
+void helloWorld();
+}  // namespace memopt
